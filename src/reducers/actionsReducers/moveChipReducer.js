d46eef7684be4fd3635a4b08b46f3a9bcec0e9d8
carlesba <carlesba.js@gmail.com>
2015-12-06 18:37:07 +0100

    Avoid movements to non highlighted tiles

--- src/reducers/actionsReducers/moveChipReducer.js
+++ src/reducers/actionsReducers/moveChipReducer.js
@@ -2,16 +2,30 @@ const isTargetTile = (tile, {row, col}) => {
   return tile.row === row && tile.col === col
 }
 
+const findTile = ({row, col}, tiles) => {
+  return tiles.find(tile => isTargetTile(tile, {row, col}))
+}
+
+const isAllowedTile = (targetTile) => {
+  return targetTile.highlighted
+}
+
 // update chips in tiles when moving chips
 export const moveChipReducer = (tiles, action) => {
   const {currentPosition, nextPosition, chipId} = action
-  return tiles.map((tile) => {
-    if (isTargetTile(tile, currentPosition)) {
-      return Object.assign({}, tile, {chipId: null})
-    } else if (isTargetTile(tile, nextPosition)) {
-      return Object.assign({}, tile, {chipId: chipId})
-    } else {
-      return tile
-    }
-  })
+  const targetTile = findTile(nextPosition, tiles)
+
+  if (isAllowedTile(targetTile)) {
+    return tiles.map((tile) => {
+      if (isTargetTile(tile, currentPosition)) {
+        return Object.assign({}, tile, {chipId: null})
+      } else if (isTargetTile(tile, nextPosition)) {
+        return Object.assign({}, tile, {chipId: chipId})
+      } else {
+        return tile
+      }
+    })
+  } else {
+    return tiles
+  }
 }
