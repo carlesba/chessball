7411503e09e48dc68ee734ec54d80c34f5f1f5e8
carlesba <carlesba.js@gmail.com>
2015-12-23 21:18:36 +0100

    Add turns owner on status component

--- src/components/Status.js
+++ src/components/Status.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Status = ({game}) => {
   const {scoreTeamA, scoreTeamB, turnOwner} = game
   const score = `${scoreTeamA}-${scoreTeamB}`
+  const playerTurn = turnOwner === 0
+    ? 'Red\'s turn'
+    : 'Blue\'s turn'
   return (
     <div className='status'>
         <div>{score}</div>
-        <div>turn {turnOwner}</div>
+        <div>{playerTurn}</div>
     </div>
   )
 }
