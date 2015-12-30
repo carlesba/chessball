e45601379b81ee923e82e3b1a91edfd709f3a2c8
carlesba <carlesba.js@gmail.com>
2015-12-30 18:19:49 +0100

    Refactor showMovesReducer that allows easy composition

--- src/reducers/showMovesReducer.js
+++ src/reducers/showMovesReducer.js
@@ -4,46 +4,78 @@ import {
 } from '../utils/constants'
 import { calculatePositionOwner } from '../utils/position'
 
-const positionAllowedInBoard = ({row, col}, board, isBall) => {
-  const tile = board[row] && board[row][col]
-  if (!tile) return false
-  return isBall
-    ? tile.kind !== 'blank'
-    : tile.kind === 'game'
+const showMovesReducer = (state, action) => {
+  const {chips, game: {ballPasses}, board} = state
+  const {chipId} = action
+  const chip = chips.find(chip => chip.chipId === chipId)
+  const isBall = chip.kind === 'ball'
+  const positionFilterer = getPositionFilterer(isBall, ballPasses, board, chips)
+  return { movements: calculatePositionsFrom(chip, isBall, positionFilterer) }
 }
 
-const positionIsChipFree = (position, chips) => {
+const getPositionFilterer = (isBall, ballPasses, board, chips) => {
+  if (isBall && ballPasses === 0) {
+    return neutralPositionFilterer(board, chips)
+  } else if (isBall) {
+    return ballPositionFilterer(board, chips)
+  } else {
+    return chipPositionFilterer(board, chips)
+  }
+}
+
+const neutralPositionFilterer = (board, chips) => (position, stopLooking) => {
+  if (
+    isAllowedForBallOnBoard(position, board) &&
+    isChipFree(position, chips) &&
+    isNeutral(position, chips)
+  ) {
+    return true
+  }
+  return false
+}
+const ballPositionFilterer = (board, chips) => (position, stopLooking) => {
+  if (
+    isAllowedForBallOnBoard(position, board) &&
+    isChipFree(position, chips)
+  ) {
+    return true
+  }
+  return false
+}
+
+const chipPositionFilterer = (board, chips) => (position, stopLooking) => {
+  if (
+    isAllowedForChipsOnBoard(position, board) &&
+    isChipFree(position, chips)
+  ) {
+    return true
+  }
+  stopLooking()
+  return false
+}
+
+const isAllowedForChipsOnBoard = ({row, col}, board) => {
+  const tile = board[row] && board[row][col]
+  if (!tile) return false
+  return tile.kind !== 'blank' && tile.kind !== 'goal'
+}
+
+const isAllowedForBallOnBoard = ({row, col}, board) => {
+  const tile = board[row] && board[row][col]
+  if (!tile) return false
+  return tile.kind !== 'blank'
+}
+
+const isChipFree = (position, chips) => {
   return chips.findIndex(({row, col}) => {
     return position.row === row && position.col === col
   }) < 0
 }
 
-const positionIsAllowed = (position, chips, board, isBall) => {
-  return positionIsChipFree(position, chips) && positionAllowedInBoard(position, board, isBall)
+const isNeutral = (position, chips) => {
+  return calculatePositionOwner(position, chips) === null
 }
 
-const getAvailablePositions = (origin, increment, isBall, checkPositionCallback) => {
-  let positions = []
-  const maxMovements = isBall
-    ? MAX_BALL_MOVE
-    : MAX_PLAYER_MOVE
-  let current = {
-    row: origin.row,
-    col: origin.col
-  }
-  for (let i = 0, keepLooking = true; i < maxMovements && keepLooking; i++) {
-    current = {
-      row: current.row + increment.row,
-      col: current.col + increment.col
-    }
-    if (checkPositionCallback(current)) {
-      positions.push(current)
-    } else {
-      keepLooking = isBall // ball won't be stopped by another chip, can jump
-    }
-  }
-  return positions
-}
 const incrementals = [
   {row: 1, col: 0},
   {row: -1, col: 0},
@@ -54,29 +86,34 @@ const incrementals = [
   {row: -1, col: -1},
   {row: -1, col: 1}
 ]
+
 const calculatePositionsFrom = (origin, isBall, filterPositions) => {
+  const times = isBall
+    ? MAX_BALL_MOVE
+    : MAX_PLAYER_MOVE
   return incrementals.reduce((positions, increment) => {
     return positions.concat(
-      getAvailablePositions(origin, increment, isBall, filterPositions)
+      getAvailablePositions(origin, increment, times, filterPositions)
     )
   }, [])
 }
-const showMovesReducer = (state, action) => {
-  const {chips, game: {ballPasses}, board} = state
-  const {chipId} = action
-  const chip = chips.find(chip => chip.chipId === chipId)
-  const isBall = chip.kind === 'ball'
-  const keepAllowedPositions = (position) => {
-    return positionIsAllowed(position, chips, board, isBall)
+
+const getAvailablePositions = (origin, increment, times, isValidPosition) => {
+  let positions = []
+  let current = {
+    row: origin.row,
+    col: origin.col
   }
-  const keepNeutralAllowedPositions = (position) => {
-    return positionIsAllowed(position, chips, board, isBall) &&
-      calculatePositionOwner(position, chips) === null
+  let keep = true
+  const stopper = () => keep = false
+  for (let i = 0; i < times && keep; i++) {
+    current = {
+      row: current.row + increment.row,
+      col: current.col + increment.col
+    }
+    if (isValidPosition(current, stopper)) positions.push(current)
   }
-  const filterPositions = isBall && ballPasses === 0
-    ? keepNeutralAllowedPositions
-    : keepAllowedPositions
-  return { movements: calculatePositionsFrom(chip, isBall, filterPositions) }
+  return positions
 }
 
 export default showMovesReducer
