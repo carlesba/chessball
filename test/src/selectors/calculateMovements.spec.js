525354071b6a23ee7a39b85d7a465e6a9b464ce9
carlesba <carlesba.js@gmail.com>
2016-05-29 17:30:20 +0200

    Define calculateMomvents selector tests

--- test/src/selectors/calculateMovements.spec.js
+++ test/src/selectors/calculateMovements.spec.js
@@ -1,1 +1,23 @@
-describe('calculateMovements:\n', () => {})
+import expect from 'expect'
+import calculateMovements from 'src/selectors/calculateMovements'
+import chipsReducer from 'src/reducers/chips'
+
+describe('calculateMovements:\n', () => {
+  describe('when no chip is selected', () => {
+    it('returns an empty array', () => {
+      const noChipSelected = chipsReducer()
+      const target = calculateMovements(noChipSelected)
+      expect(target.length).toBe(0)
+    })
+  })
+  describe('when the ball is selected', () => {
+    it('returns all positions in a distance of 4', () => {})
+    it('doesn\'t positions outside the board', () => {})
+    it('doesn\'t positions where another chip is placed', () => {})
+  })
+  describe('when a player chip is selected', () => {
+    it('returns all positions in a distance of 2', () => {})
+    it('doesn\'t positions outside the board', () => {})
+    it('doesn\'t positions after finding an obstacle', () => {})
+  })
+})
