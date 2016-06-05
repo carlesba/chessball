971e2e151fea25b817eb2b068d6cac565a70e5a5
carlesba <carlesba.js@gmail.com>
2016-06-05 11:51:27 +0200

    Calculate basic movements

--- app/src/selectors/calculateMovements.js
+++ app/src/selectors/calculateMovements.js
@@ -1,3 +1,42 @@
-export function calculateMovements (chips) {
-  return []
+export default function calculateMovements (chips) {
+  const selectedChip = chips.find(({isSelected}) => isSelected)
+  if (!selectedChip) return []
+  if (selectedChip.type === 'ball') return calculateBallMovements(chips)
+  else calculatePlayerMovements(chips, selectedChip)
 }
+
+const DIRECTIONS = [
+  [0, 1], [1, 0], [1, 1],
+  [0, -1], [-1, 0], [-1, -1],
+  [-1, 1], [1, -1]
+]
+const BALL_DISTANCE = 4
+const PLAYER_DISTANCE = 2
+
+const calculateBallMovements = ([ball, ...players]) => {
+  const source = ball.position
+  return DIRECTIONS.reduce((positions, increment) => {
+    return positions.concat(getPositions(source, increment, BALL_DISTANCE))
+  }, [])
+}
+
+const calculatePlayerMovements = (chips, selectedChip) => {
+  const source = selectedChip.position
+  return DIRECTIONS.reduce((positions, increment) => {
+    return positions.concat(getPositions(source, increment, PLAYER_DISTANCE))
+  }, [])
+}
+
+const getPositions = (source, increment, distance) => {
+  let step = distance
+  let previous = source
+  let output = []
+  while (step > 0) {
+    previous = applyIncrement(previous, increment)
+    output.push(previous)
+    step -= 1
+  }
+  return output
+}
+
+const applyIncrement = ([a, b], [incr0, incr1]) => ([a + incr0, b + incr1])
--- test/src/selectors/calculateMovements.spec.js
+++ test/src/selectors/calculateMovements.spec.js
@@ -5,15 +5,34 @@ import chipsReducer from 'src/reducers/chips'
 describe('calculateMovements:\n', () => {
   describe('when no chip is selected', () => {
     it('returns an empty array', () => {
-      // const noChipSelected = chipsReducer()
-      // const target = calculateMovements(noChipSelected)
-      // expect(target.length).toBe(0)
+      const noChipSelected = chipsReducer()
+      const target = calculateMovements(noChipSelected)
+      expect(target.length).toBe(0)
     })
   })
   describe('when the ball is selected', () => {
-    it('returns all positions in a distance of 4', () => {})
-    it('doesn\'t positions outside the board', () => {})
-    it('doesn\'t positions where another chip is placed', () => {})
+    it('returns all positions in a distance of 4', () => {
+      const ballSelected = chipsReducer().setIn([0, 'isSelected'], true)
+      const target = calculateMovements(ballSelected)
+      expect(target.length).toBe(32, 'bad length')
+      const expectedMovements = [
+        [6, 5], [5, 5], [4, 5], [3, 5],
+        [8, 5], [9, 5], [10, 5], [11, 5],
+        [7, 4], [7, 3], [7, 2], [7, 1],
+        [7, 6], [7, 7], [7, 8], [7, 9],
+        [6, 4], [5, 3], [4, 2], [3, 1],
+        [6, 6], [5, 7], [4, 8], [3, 9],
+        [8, 4], [9, 3], [10, 2], [11, 1],
+        [8, 6], [9, 7], [10, 8], [11, 9]
+      ]
+      expectedMovements.forEach(([m0, m1]) => {
+        const move = target.find(([a, b]) => m0 === a && m1 === b)
+        expect(move).toBeTruthy(`${[m0, m1]} not found`)
+      })
+    })
+    it('doesn\'t return positions outside the board', () => {})
+    it('can return goal positions', () => {})
+    it('doesn\'t return positions where another chip is placed', () => {})
   })
   describe('when a player chip is selected', () => {
     it('returns all positions in a distance of 2', () => {})
