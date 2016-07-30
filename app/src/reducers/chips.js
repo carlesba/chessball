dc39c91be957397eefbce717566c5be9251c76bf
carlesba <carlesba.mail@gmail.com>
2016-07-30 23:26:43 +0200

    Fix select chip

--- app/src/reducers/chips.js
+++ app/src/reducers/chips.js
@@ -29,16 +29,10 @@ const initialState = createChips([
 
 const reducerMap = {
   [SELECT_CHIP]: (state, {chipId}) => {
-    // const prevSelectedChipIndex = state.findIndex(({isSelected}) => isSelected)
-    // const targetChipIndex = state.findIndex(({id}) => id === chipId)
-    // const unselected = prevSelectedChipIndex < 0
-    //   ? state
-    //   : state.setIn([prevSelectedChipIndex, 'isSelected'], false)
-    // const selected = targetChipIndex < 0
-    //   ? unselected
-    //   : unselected.setIn([targetChipIndex, 'isSelected'], true)
-    // return selected
-    return state.unselectChip().selectChip(chipId)
+    const selectedChip = state.getSelectedChip()
+    if (!selectedChip) return state.selectChip(chipId)
+    if (selectedChip.id === chipId) return state.unselectChip()
+    else state.unselectChip().selectChip(chipId)
   },
   [MOVE_SELECTED_CHIP]: (state, {position, team}) => {
     const selectedPlayerIndex = state.findIndex(({isSelected}) => isSelected)
