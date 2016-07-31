198574fa976dd2af35a517c9eb5882dc629bdbe4
carlesba <carlesba.mail@gmail.com>
2016-07-31 11:40:32 +0200

    Simplify pass count reducer

--- app/src/reducers/pass-count.js
+++ app/src/reducers/pass-count.js
@@ -1,20 +1,8 @@
-import createReducer from 'src/lib/createReducer'
-import {
-  SCORE,
-  MOVE_PLAYER,
-  MOVE_BALL,
-  PASS_BALL,
-  MOVE_BALL_TO_BONUS
-} from 'src/constants'
+import { PASS_BALL } from 'src/constants'
 
 const initialState = 0
 
-const reducerMap = {
-  [PASS_BALL]: (state) => state + 1,
-  [MOVE_PLAYER]: (state) => initialState,
-  [MOVE_BALL]: (state) => initialState,
-  [MOVE_BALL_TO_BONUS]: (state) => initialState,
-  [SCORE]: (state) => initialState
+export default function passCountReducer (state = initialState, action) {
+  if (action.type === PASS_BALL) return state + 1
+  else return initialState
 }
-
-export default createReducer(reducerMap, initialState)
