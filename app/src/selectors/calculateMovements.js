0544bdcad5c7180cde80681cd5227ac363b18dd5
carlesba <carlesba.mail@gmail.com>
2016-08-07 16:10:53 +0200

    Clean code

--- app/src/selectors/calculateMovements.js
+++ app/src/selectors/calculateMovements.js
@@ -17,12 +17,9 @@ function calculateBallMovements (state, actions) {
   const {chips} = state
   const ballPosition = chips.getBall().position
   const usedPositions = chips.getPositions()
-  // const keeperSaves = chips.getKeepersSaves()
   return positionsFromTo(ballPosition, BALL_DISTANCE)
     .filter((p) => p.isValidBallPosition(usedPositions))
-    // .filter(keeperSavePosition(ballPosition, keeperSaves))
     .map(createMovementWithActions(actions, state))
-    // .filter(movementsWithoutAction)
 }
 
 function calculatePlayerMovements (state, actions) {
@@ -30,34 +27,13 @@ function calculatePlayerMovements (state, actions) {
   const selectedChip = chips.getSelectedChip()
   const origin = selectedChip.position
   const usedPositions = chips.getPositions()
-  // const keeperHands = chips.getKeepersHandsPositions()
   return positionsFromTo(origin, PLAYER_DISTANCE)
     .filter((p) => p.isValidPlayerPosition(usedPositions))
-    // .filter((p) => p.isPathFreeUntil(origin, usedPositions))
-    // .filter((p) => p.isPathFreeUntil(origin, keeperHands))
-    // .filter(notKeeperHandsPosition(selectedChip, keeperHands))
     .map(createMovementWithActions(actions, state))
-    // .filter(movementsWithoutAction)
 }
-
-// const isValidBallPosition = (usedPositions) => (pos) =>
-//   pos.isValidBallPosition(usedPositions)
-//
-// const isValidPlayerPosition = (usedPositions) => (pos) =>
-//   pos.isValidPlayerPosition(usedPositions)
-//
-// const keeperSavePosition = (origin, saves) => (pos) => pos
 
 const createMovementWithActions = (actions, state) => (pos) =>
   createMovement(pos, actions, state)
-
-// const notKeeperHandsPosition = (caller, keeperHands) => (pos) => {
-//   if (caller.isKeeper) return pos
-//   // TODO: filter positions when keeper would be next to other chip inside area
-//   return !pos.isIn(keeperHands)
-// }
-
-const movementsWithoutAction = (movement) => !!movement.onClick
 
 const DIRECTIONS = [
   [0, 1], [1, 0], [1, 1],
